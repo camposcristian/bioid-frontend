ab650c529c6d3b5a3f588cd84c98160cdafc5ddf
Cristian Campos <campos@strands.com>
2015-09-21 00:40:09 -0300

    Demo para Luson Seguridad

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,8 @@
  *
  * Main module of the application.
  */
+
+// #1b75bb
 angular
   .module('bioidApp', [
     'ngAnimate',
@@ -19,8 +21,14 @@ angular
     'btford.socket-io'
   ])
   .config(function($routeProvider, $mdThemingProvider) {
+    $mdThemingProvider.definePalette('amazingPaletteName', { '50': 'd1e3f1', '100': 'bad5ea', '200': 'a3c7e3', '300': '8dbadd', '400': '76acd6', '500': '5f9ecf', '600': '4890c8', '700': '3182c1', '800': '1b75bb', '900': '1869a8', 'A100': '155d95', 'A200': '125182', 'A400': '104670', 'A700': '0d3a5d', 'contrastDefaultColor': 'light', // whether, by default, text (contrast) // on this palette should be dark or light
+
+'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+'200', '300', '400', 'A100'],
+'contrastLightColors': undefined    // could also specify this if default was 'dark'
+});
     $mdThemingProvider.theme('default')
-      .primaryPalette('teal')
+      .primaryPalette('amazingPaletteName')
       .accentPalette('light-blue');
     $routeProvider
       .when('/', {
--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -13,6 +13,7 @@ angular.module('bioidApp')
 			{
 				face: "https://material.angularjs.org/latest/img/list/60.jpeg",
 				what: 'Juan Carlos Vallejos',
+				dni: "35550990",
 				who: 'Entró a las 10AM',
 				when: 'Hora estimada de salida: 12PM',
 				notes: "Motivo: Personal Trainer"
@@ -20,6 +21,34 @@ angular.module('bioidApp')
 			{
 				face: "https://en.gravatar.com/userimage/64217526/6649c70a54ba475dda143218b65c19fd.jpeg",
 				what: 'José Brugada',
+				dni: "35367687",
+				who: 'Entró a las 9AM, Destino: Depto 8 Piso 10',
+				when: 'Hora estimada de salida: 10AM',
+				notes: "Motivo: Plomero"
+			},
+			{
+				face: "https://material.angularjs.org/latest/img/list/60.jpeg",
+				what: 'Sebastián Matrtínez',
+				dni: "353660112",
+				who: 'Entró a las 9AM, Destino: Depto 8 Piso 10',
+				when: 'Hora estimada de salida: 10AM',
+				notes: "Motivo: Gasista"
+			},
+		];
+
+		$scope.visitors = [
+			{
+				face: "https://material.angularjs.org/latest/img/list/60.jpeg",
+				what: 'Juan Carlos Vallejos',
+				dni: "35550990",
+				who: 'Entró a las 10AM',
+				when: 'Hora estimada de salida: 12PM',
+				notes: "Motivo: Personal Trainer"
+			},
+			{
+				face: "https://en.gravatar.com/userimage/64217526/6649c70a54ba475dda143218b65c19fd.jpeg",
+				what: 'José Brugada',
+				dni: "35367687",
 				who: 'Entró a las 9AM, Destino: Depto 8 Piso 10',
 				when: 'Hora estimada de salida: 10AM',
 				notes: "Motivo: Plomero"
@@ -27,15 +56,20 @@ angular.module('bioidApp')
 			{
 				face: "https://material.angularjs.org/latest/img/list/60.jpeg",
 				what: 'Sebastián Zorrilla',
+				dni: "353660112",
 				who: 'Entró a las 9AM, Destino: Depto 8 Piso 10',
 				when: 'Hora estimada de salida: 10AM',
 				notes: "Motivo: Gasista"
 			},
 		];
 
+		$scope.sacar = function (item) {
+			$scope.todos.pop(item);
+		}
+
+
 		var self = this;
 		// list of `state` value/display objects
-		self.states = loadAll();
 		self.selectedItem = null;
 		self.searchText = null;
 		self.querySearch = querySearch;
@@ -47,34 +81,86 @@ angular.module('bioidApp')
 		 * remote dataservice call.
 		 */
 		function querySearch(query) {
-			var results = query ? self.states.filter(createFilterFor(query)) : [];
+			var results = query ? $scope.visitors.filter(createFilterFor(query)) : [];
 			return results;
 		}
-		/**
-		 * Build `states` list of key/value pairs
-		 */
-		function loadAll() {
-			var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-              Wisconsin, Wyoming';
-			return allStates.split(/, +/g).map(function (state) {
-				return {
-					value: state.toLowerCase(),
-					display: state
-				};
-			});
-		}
+
 		/**
 		 * Create filter function for a query string
 		 */
 		function createFilterFor(query) {
 			var lowercaseQuery = angular.lowercase(query);
 			return function filterFn(state) {
-				return (state.value.indexOf(lowercaseQuery) === 0);
+				return (state.dni.indexOf(lowercaseQuery) === 0);
 			};
-	}
+		}
+
+		mySocket.on('connect_error', function () {
+			mySocket.removeAllListeners('connect_error');
+			$mdDialog.show({
+				controller: ErrorController,
+				templateUrl: '/views/verify.dialog.html',
+				clickOutsideToClose: false
+			});
+		});
+
+		$scope.verify = function (markId, ev) {
+			// ev.preventDefault()
+			mySocket.on('connect_error', function () {
+				mySocket.removeAllListeners('connect_error');
+				$mdDialog.show({
+					controller: ErrorController,
+					templateUrl: '/views/verify.dialog.html',
+					clickOutsideToClose: false
+				});
+			});
+
+			$mdDialog.show({
+				controller: VerificationController,
+				templateUrl: '/views/verify.dialog.html',
+				targetEvent: ev,
+				clickOutsideToClose: false
+			});
+			mySocket.emit('verify', parseInt(markId));
+		};
+
+		function ErrorController($scope, $mdDialog) {
+			$scope.content = 'Inicie el programa bioid para continuar';
+			$scope.isOk = false;
+			$scope.image = '/images/thumb43.svg';
+			$scope.thumbClass = 'md-warn';
+
+			$scope.hide = function () {
+				mySocket.emit('cancel');
+				$mdDialog.hide();
+			};
+		}
+
+		function VerificationController($scope, $mdDialog, mySocket) {
+			$scope.content = 'Ingrese su huella';
+			$scope.progress = 30;
+			$scope.image = '/images/thumb45.svg';
+			$scope.thumbClass = 'md-accent';
+
+			mySocket.on('data', function (data) {
+				$scope.content = data.content;
+				$scope.progress = data.progress;
+				if (data.progress === 100) {
+					mySocket.removeAllListeners();
+					$scope.isOk = true;
+					$scope.thumbClass = 'md-primary';
+					$scope.image = 'http://www.gravatar.com/avatar/' + data.email;
+				} else if (data.progress === -1) {
+					$scope.image = '/images/thumb43.svg';
+					$scope.thumbClass = 'md-warn';
+				}
+			});
+
+			$scope.hide = function () {
+				mySocket.emit('cancel');
+				$mdDialog.hide();
+			};
+
+		}
+
 	});
